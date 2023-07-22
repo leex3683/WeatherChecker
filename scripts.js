ac870f9e20ea09cd39b93080169ec331da28f8d6
unknown <leex3683@umn.edu>
2023-07-22 16:36:02 -0500

    finished weather cards, changed style to bootstrap 4, added a non-functioning dropdown

--- scripts.js
+++ scripts.js
@@ -3,6 +3,10 @@ let inputElSearch = document.querySelector('.searchInput');
 let sectionElForecast = document.querySelector('.forecast');
 let buttonEl = document.querySelector('.submitInput');
 let weatherHeaderEl = document.querySelector('.weather-header');
+let todayTempEl = document.querySelector('.today-temp');
+let todayWindEl = document.querySelector('.today-wind');
+let todayHumidityEl = document.querySelector('.today-humidity');
+let todayPicEl = document.querySelector('.today-pic');
 
 today = dayjs();
 // console.log(today.add(1,'day').format('dddd, MMMM, D'));
@@ -16,46 +20,54 @@ buttonEl.addEventListener("click", function () {
     .then(function (data) {
       console.log('Fetch Response \n-------------');
       console.log(data.list);
-      let h4El = document.createElement('h4');
+
       //wipe forecast cards
       sectionElForecast.textContent = "";
       weatherHeaderEl.textContent = '';
-      weatherHeaderEl.appendChild(h4El).textContent = inputElSearch.value + " | " + today.format('dddd, MMMM, D');
-
-
+      todayPicEl.textContent = '';
 
       for (let i = 0; i < data.list.length; i++) {
-
-        for (let j = 1; j < 6; j++) {
-          if (data.list[i].dt_txt.includes(today.add(j, 'day').format('YYYY-MM-D')) && data.list[i].dt_txt.includes('12:00:00')) {
-
-            console.log(data.list[i].dt_txt);
-
-            //create forecast cards
-            let divEl1 = document.createElement('div');
-            let divChEl1 = document.createElement('div');
-            let divChEl2 = document.createElement('div');
-
-            let h5El = document.createElement('h5');
-            let pEl1 = document.createElement('p');
-            let pEl2 = document.createElement('p');
-            divChEl2.appendChild(h5El).textContent = "Temp: " + Math.round((data.list[i].main.temp - 273.15) * (9 / 5) + 32) + "°F";
-            divChEl2.appendChild(pEl1).textContent = "wind: " + data.list[i].wind.speed + " MPH";
-            divChEl2.appendChild(pEl2).textContent = "humidity: " + data.list[i].main.humidity + '%';
-
-            divEl1.appendChild(divChEl1).textContent = data.list[i].dt_txt;
-            divEl1.appendChild(divChEl2);
-
-            sectionElForecast.appendChild(divEl1);
-            divEl1.setAttribute('class', 'card bg-light mb-3');
-            divChEl1.setAttribute('class', 'card-header');
-            divEl1.setAttribute('style', 'max-width: 18rem;');
-            divChEl2.setAttribute('class', 'card-body');
-            h5El.setAttribute('class', 'card-title');
-            pEl1.setAttribute('class', 'card-text');
-            pEl2.setAttribute('class', 'card-text');
-          };
-
+        for (let j = 0; j < 6; j++) {
+          if (j == 0 && i == 0) {
+            //Create Today's weather card (j = 0)
+            let h4El = document.createElement('h4');
+            weatherHeaderEl.appendChild(h4El).textContent = inputElSearch.value + " | " + data.list[i].dt_txt;
+            todayTempEl.textContent = "Temp: " + Math.round((data.list[i].main.temp - 273.15) * (9 / 5) + 32) + "°F";
+            todayWindEl.textContent = "wind: " + data.list[i].wind.speed + " MPH";
+            todayHumidityEl.textContent = "humidity: " + data.list[i].main.humidity + '%';
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', 'https://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '@2x.png');
+            imgEl.setAttribute('alt', 'weather icon');
+            todayPicEl.appendChild(imgEl);
+          }
+          else {
+            if (data.list[i].dt_txt.includes(today.add(j, 'day').format('YYYY-MM-D')) && data.list[i].dt_txt.includes('12:00:00')) {
+              //create forecast cards (j = [1,6])
+              let divEl1 = document.createElement('div');
+              let divChEl1 = document.createElement('div');
+              let divChEl2 = document.createElement('div');
+              let h5El = document.createElement('h5');
+              let pEl1 = document.createElement('p');
+              let pEl2 = document.createElement('p');
+              let imgEl = document.createElement('img');
+              imgEl.setAttribute('src', 'https://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '@2x.png');
+              imgEl.setAttribute('alt', 'weather icon');
+              divChEl2.appendChild(imgEl);
+              divChEl2.appendChild(h5El).textContent = "Temp: " + Math.round((data.list[i].main.temp - 273.15) * (9 / 5) + 32) + "°F";
+              divChEl2.appendChild(pEl1).textContent = "wind: " + data.list[i].wind.speed + " MPH";
+              divChEl2.appendChild(pEl2).textContent = "humidity: " + data.list[i].main.humidity + '%';
+              divEl1.appendChild(divChEl1).textContent = data.list[i].dt_txt;
+              divEl1.appendChild(divChEl2);
+              sectionElForecast.appendChild(divEl1);
+              divEl1.setAttribute('class', 'card bg-light mb-3');
+              divChEl1.setAttribute('class', 'card-header');
+              divEl1.setAttribute('style', 'max-width: 18rem;');
+              divChEl2.setAttribute('class', 'card-body');
+              h5El.setAttribute('class', 'card-title');
+              pEl1.setAttribute('class', 'card-text');
+              pEl2.setAttribute('class', 'card-text');
+            };
+          }
         }
 
       }
